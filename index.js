59c1d63eb9153660623fc38c1e90bb2a1d13a2b6
mbower386 <mbower386@gmail.com>
2019-12-10 18:21:37 -0800

    added react elements

---
+++ index.js
@@ -0,0 +1,13 @@
+const helloReact = React.createElement('h1', null, "Hello React");
+
+const thingsDo = React.createElement('h2', null, "Hello React");
+
+const helloReact = React.createElement('h1', null, "Hello React");
+
+const helloReact = React.createElement('h1', null, "Hello React");
+
+const helloReact = React.createElement('h1', null, "Hello React");
+
+const helloReact = React.createElement('h1', null, "Hello React");
+
+ReactDOM.render(helloReact, document.getElementById('root'));
